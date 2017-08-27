f2baf047bf4d67c5d5e57aa5e3c50609b50d4095
Emre Ay <eay@kth.se>
2017-08-27 20:42:48 +0200

    UI improved, new functionalities for back end

--- cyclops.py
+++ cyclops.py
@@ -5,12 +5,17 @@
 
 from scipy.spatial import distance as dist
 from imutils import perspective
-# from imutils import contours
 import numpy as np
 import argparse
 import imutils
 import cv2
 import yaml
+
+class Keys:
+    space = 1048608
+    esc = 1048603
+    enter = 1048586
+    c = 1048675
 
 # Class for estimating the scale for pixel/meter, assuming a perpendicular
 # view on a planar scene. The estimation function is based on
@@ -18,7 +23,6 @@ import yaml
 # http://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/
 class Scaler:
     "Class for estimating the scale for pixel/meter, assuming a perpendicular view on a planar scene. The estimation function is based on Adrian Rosebrock's implementation available at http://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/"
-    keys = {"space":1048608, "c":1048675, "esc":1048603, "enter":1048586}
     def __init__(self, cam_src = 0, param_src = None):
         self.image = np.zeros((1,1,1), np.uint8)
         self.prc_image = np.zeros((1,1,1), np.uint8)
@@ -29,6 +33,7 @@ class Scaler:
         self.cropped = False
         self.undistort = False
         self.reference_width = 0.297
+        self.keys = Keys()
         self.vc = cv2.VideoCapture(cam_src)
         self.main_window = "Scaler"
         self.cropped_window = "Cropped"
@@ -102,7 +107,7 @@ class Scaler:
 
         k = cv2.waitKey(50)
         cropped_image = self.prc_image
-        if k == self.keys["space"] or self.cropped:
+        if k == self.keys.space or self.cropped:
             self.cropped = True
             if len(self.roi) == 2:
                 (r1,r2),(r3,r4) = self.roi
@@ -114,21 +119,21 @@ class Scaler:
                 cv2.namedWindow(self.cropped_window, cv2.WINDOW_AUTOSIZE)
                 cv2.imshow(self.cropped_window, cropped_image)
 
-        if k == self.keys["c"] and self.cropped:
+        if k == self.keys.c and self.cropped:
             th = int(np.mean(cropped_image))
             ret, thresh = cv2.threshold(cropped_image, th, 255, cv2.THRESH_BINARY)
             cv2.namedWindow(self.binary_window, cv2.WINDOW_AUTOSIZE)
             cv2.imshow(self.binary_window, thresh)
             self.estimate_scale(thresh)
 
-        if k == self.keys["enter"]:
+        if k == self.keys.enter:
             if self.pixel_scale:
                 self.destroy_windows()
                 self.done = True
             else:
                 print "Operation is not done yet."
 
-        if k == self.keys["esc"]:
+        if k == self.keys.esc:
             self.destroy_windows()
             self.quit = True
 
@@ -139,9 +144,6 @@ class Scaler:
         cnts = cv2.findContours(edge.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
         cnts = cnts[0] if imutils.is_cv2() else cnts[1]
 
-        # sort the contours from left-to-right and initialize the
-        # 'pixels per metric' calibration variable
-        # (cnts, _) = contours.sort_contours(cnts)
         # loop over the contours individually
         for c in cnts:
             if cv2.contourArea(c) < 100:
@@ -206,3 +208,120 @@ class Scaler:
                 self.process(self.image)
         self.vc.release()
         return self.pixel_scale
+
+class MemberInitializer:
+    def __init__(self, cam_src = 0):
+        self.color = None
+        self.done = False
+        self.quit = False
+        self.drag = False
+        self.cropped = False
+        self.keys = Keys()
+        self.roi = []
+        self.image = np.zeros((1,1,1), np.uint8)
+        self.main_window = "Add Member"
+        self.cropped_window = "Selected Area For Member Color"
+        self.color_window = "Selected Member Color"
+        self.vc = cv2.VideoCapture(cam_src)
+
+    def destroy_windows(self):
+        cv2.destroyWindow(self.main_window)
+        cv2.destroyWindow(self.cropped_window)
+        cv2.destroyWindow(self.color_window)
+
+    def mouse_callback(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN and not self.drag:
+            self.roi = [(x,y)]
+            self.drag = True
+
+        if event == cv2.EVENT_MOUSEMOVE and self.drag:
+            temp_img = self.image.copy()
+            cv2.rectangle(temp_img, self.roi[0], (x,y), (0, 255, 0), 2)
+            cv2.imshow(self.main_window, temp_img)
+
+        if event == cv2.EVENT_LBUTTONUP and self.drag:
+            self.roi.append((x,y))
+            temp_img = self.image.copy()
+            cv2.rectangle(temp_img, self.roi[0], (x,y), (0, 255, 0), 2)
+            cv2.imshow(self.main_window, temp_img)
+            self.drag = False
+
+        if event == cv2.EVENT_RBUTTONDOWN:
+            self.roi = []
+            self.drag = False
+            self.cropped = False
+            cv2.destroyWindow(self.cropped_window)
+            cv2.destroyWindow(self.color_window)
+
+    def add_padding(self, img, padding=50):
+        offset = padding/2
+        # background image to show the selected area
+        back = np.zeros((img.shape[0]+padding, img.shape[1]+padding, img.shape[2]), np.uint8)
+        # overlaying cropped area on background image
+        back[offset:offset+img.shape[0],offset:offset+img.shape[1]] = img
+        return back
+
+    def get_member_color(self):
+        while not self.done and not self.quit:
+            if self.vc.isOpened():
+                _ , self.image = self.vc.read()
+                cv2.namedWindow(self.main_window, cv2.WINDOW_AUTOSIZE)
+                cv2.setMouseCallback(self.main_window, self.mouse_callback)
+                k = cv2.waitKey(50)
+                crop = self.image
+                if k == self.keys.space or self.cropped:
+                    self.cropped = True
+                    if len(self.roi) == 2:
+                        (r1,r2),(r3,r4) = self.roi
+                        y1 = min(r1,r3)
+                        y2 = max(r1,r3)
+                        x1 = min(r2,r4)
+                        x2 = max(r2,r4)
+                        crop = crop[x1:x2,y1:y2]
+                        crop_pad = self.add_padding(crop)
+                        cv2.namedWindow(self.cropped_window, cv2.WINDOW_AUTOSIZE)
+                        cv2.imshow(self.cropped_window, crop_pad)
+
+                if len(self.roi) == 2:
+                    temp_img = self.image.copy()
+                    cv2.rectangle(temp_img, self.roi[0], self.roi[1], (0, 0, 255), 2)
+                    cv2.imshow(self.main_window, temp_img)
+                else:
+                    cv2.imshow(self.main_window, self.image)
+
+                if k == self.keys.c and self.cropped:
+                    c = cv2.mean(crop)
+                    color_img = crop
+                    color_img[:] = (c[0],c[1],c[2])
+                    color_pad = self.add_padding(color_img)
+                    cv2.namedWindow(self.color_window, cv2.WINDOW_AUTOSIZE)
+                    cv2.imshow(self.color_window, color_pad)
+                    self.color = c
+                    print "Average BGR for member id: {}".format(self.color)
+
+                if k == self.keys.enter:
+                    if self.color:
+                        self.destroy_windows()
+                        self.done = True
+                    else:
+                        print "Operation not done yet."
+
+                if k == self.keys.esc:
+                    self.destroy_windows()
+                    self.quit = True
+            else:
+                print "vc not opened"
+        self.vc.release()
+        return self.color
+
+class Member:
+    members = int(0)
+    def __init__(self):
+        self.color = None
+        self.initializer = MemberInitializer()
+        Member.members += 1
+        self.idx = Member.members
+        # self.idx = self.__idx
+    def get_color(self):
+        self.color = self.initializer.get_member_color()
+        return self.color

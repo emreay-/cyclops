cd97833864e367bc0e35bce1fab611b75663d0de
Emre <eay@kth.se>
2017-10-21 15:17:51 +0100

    Refactor

--- scripts/cyclops.py
+++ scripts/cyclops.py
@@ -3,28 +3,22 @@
 # Cyclops project: Localization system with an overhead camera
 #
 
-from scipy.spatial import distance as dist
-from imutils import perspective
+from scale_estimation import scale_estimation
 import numpy as np
 import argparse
-import imutils
 import cv2
 import yaml
 
 class Keys:
-    space = 1048608
-    esc = 1048603
-    enter = 1048586
-    c = 1048675
+    space = 32
+    esc = 27
+    enter = 10
+    c = 99
 
-# Class for estimating the scale for pixel/meter, assuming a perpendicular
-# view on a planar scene. The estimation function is based on
-# Adrian Rosebrock's implementation available at
-# http://www.pyimagesearch.com/2016/03/28/measuring-size-of-objects-in-an-image-with-opencv/
 class Scaler:
     def __init__(self, cam_src = 0, param_src = None):
         self.image = np.zeros((1,1,1), np.uint8)
-        self.prc_image = np.zeros((1,1,1), np.uint8)
+        self.processed_image = np.zeros((1,1,1), np.uint8)
         self.roi = []
         self.done = False
         self.quit = False
@@ -32,7 +26,6 @@ class Scaler:
         self.cropped = False
         self.undistort = False
         self.reference_width = 0.297
-        self.keys = Keys()
         self.vc = cv2.VideoCapture(cam_src)
         self.main_window = 'Scaler'
         self.cropped_window = 'Cropped'
@@ -53,9 +46,6 @@ class Scaler:
                 self.distortion = np.array(distortion)
                 print('Camera calibration found. \nCamera matrix:\n{} \nDistortion:\n{}'.format(camera_matrix,distortion))
 
-    def midpoint(self, ptA, ptB):
-        return ((ptA[0] + ptB[0]) * 0.5, (ptA[1] + ptB[1]) * 0.5)
-
     def destroy_windows(self):
         cv2.destroyWindow(self.main_window)
         cv2.destroyWindow(self.cropped_window)
@@ -68,17 +58,17 @@ class Scaler:
             self.drag = True
 
         if event == cv2.EVENT_MOUSEMOVE and self.drag:
-            temp_img = self.prc_image.copy()
-            temp_img = cv2.cvtColor(self.prc_image, cv2.COLOR_GRAY2RGB)
-            cv2.rectangle(temp_img, self.roi[0], (x,y), (0, 255, 0), 2)
-            cv2.imshow(self.main_window, temp_img)
+            temp_image = self.processed_image.copy()
+            temp_image = cv2.cvtColor(self.processed_image, cv2.COLOR_GRAY2RGB)
+            cv2.rectangle(temp_image, self.roi[0], (x,y), (0, 255, 0), 2)
+            cv2.imshow(self.main_window, temp_image)
 
         if event == cv2.EVENT_LBUTTONUP and self.drag:
             self.roi.append((x,y))
-            temp_img = self.prc_image.copy()
-            temp_img = cv2.cvtColor(self.prc_image, cv2.COLOR_GRAY2RGB)
-            cv2.rectangle(temp_img, self.roi[0], (x,y), (0, 255, 0), 2)
-            cv2.imshow(self.main_window, temp_img)
+            temp_image = self.processed_image.copy()
+            temp_image = cv2.cvtColor(self.processed_image, cv2.COLOR_GRAY2RGB)
+            cv2.rectangle(temp_image, self.roi[0], (x,y), (0, 255, 0), 2)
+            cv2.imshow(self.main_window, temp_image)
             self.drag = False
             print('Selected region: {}'.format(self.roi))
 
@@ -90,23 +80,23 @@ class Scaler:
             cv2.destroyWindow(self.binary_window)
             cv2.destroyWindow(self.result_window)
 
-    def process(self,img):
-        self.prc_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-        self.prc_image = cv2.GaussianBlur(self.prc_image, (7, 7), 0)
+    def process(self,image):
+        self.processed_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        self.processed_image = cv2.GaussianBlur(self.processed_image, (7, 7), 0)
         cv2.namedWindow(self.main_window, cv2.WINDOW_AUTOSIZE)
         cv2.setMouseCallback(self.main_window, self.mouse_callback)
 
         if len(self.roi) == 2:
-            temp_img = self.prc_image.copy()
-            temp_img = cv2.cvtColor(self.prc_image, cv2.COLOR_GRAY2RGB)
-            cv2.rectangle(temp_img, self.roi[0], self.roi[1], (0, 0, 255), 2)
-            cv2.imshow(self.main_window, temp_img)
+            temp_image = self.processed_image.copy()
+            temp_image = cv2.cvtColor(self.processed_image, cv2.COLOR_GRAY2RGB)
+            cv2.rectangle(temp_image, self.roi[0], self.roi[1], (0, 0, 255), 2)
+            cv2.imshow(self.main_window, temp_image)
         else:
-            cv2.imshow(self.main_window, self.prc_image)
+            cv2.imshow(self.main_window, self.processed_image)
 
         k = cv2.waitKey(50)
-        cropped_image = self.prc_image
-        if k == self.keys.space or self.cropped:
+        cropped_image = self.processed_image
+        if k == Keys.space or self.cropped:
             self.cropped = True
             if len(self.roi) == 2:
                 (r1,r2),(r3,r4) = self.roi
@@ -118,85 +108,28 @@ class Scaler:
                 cv2.namedWindow(self.cropped_window, cv2.WINDOW_AUTOSIZE)
                 cv2.imshow(self.cropped_window, cropped_image)
 
-        if k == self.keys.c and self.cropped:
+        if k == Keys.c and self.cropped:
             th = int(np.mean(cropped_image))
             ret, thresh = cv2.threshold(cropped_image, th, 255, cv2.THRESH_BINARY)
             cv2.namedWindow(self.binary_window, cv2.WINDOW_AUTOSIZE)
             cv2.imshow(self.binary_window, thresh)
             self.estimate_scale(thresh)
 
-        if k == self.keys.enter:
+        if k == Keys.enter:
             if self.pixel_scale:
                 self.destroy_windows()
                 self.done = True
             else:
                 print('Operation is not done yet.')
 
-        if k == self.keys.esc:
+        if k == Keys.esc:
             self.destroy_windows()
             self.quit = True
 
-    def estimate_scale(self,img):
-        edge = cv2.Canny(img, 50, 100)
-        edge = cv2.dilate(img, None, iterations=1)
-        edge = cv2.erode(img, None, iterations=1)
-        cnts = cv2.findContours(edge.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-        cnts = cnts[0] if imutils.is_cv2() else cnts[1]
-
-        # loop over the contours individually
-        for c in cnts:
-            if cv2.contourArea(c) < 100:
-                continue
-            # bounding box
-            converted = cv2.cvtColor(img, cv2.COLOR_GRAY2RGB)
-            box = cv2.minAreaRect(c)
-            box = cv2.cv.BoxPoints(box) if imutils.is_cv2() else cv2.boxPoints(box)
-            box = np.array(box, dtype='int')
-
-            # order the points in the contour such that they appear
-            # in top-left, top-right, bottom-right, and bottom-left
-            # order, then draw the outline of the rotated bounding
-            # box
-            box = perspective.order_points(box)
-            cv2.drawContours(converted, [box.astype('int')], -1, (0, 255, 0), 2)
-            for (x, y) in box:
-                cv2.circle(converted, (int(x), int(y)), 5, (0, 0, 255), -1)
-            (tl, tr, br, bl) = box
-            (tltrX, tltrY) = self.midpoint(tl, tr)
-            (blbrX, blbrY) = self.midpoint(bl, br)
-            (tlblX, tlblY) = self.midpoint(tl, bl)
-            (trbrX, trbrY) = self.midpoint(tr, br)
-
-            # midpoint visualization
-            cv2.circle(converted, (int(tltrX), int(tltrY)), 5, (255, 0, 0), -1)
-            cv2.circle(converted, (int(blbrX), int(blbrY)), 5, (255, 0, 0), -1)
-            cv2.circle(converted, (int(tlblX), int(tlblY)), 5, (255, 0, 0), -1)
-            cv2.circle(converted, (int(trbrX), int(trbrY)), 5, (255, 0, 0), -1)
-            cv2.line(converted, (int(tltrX), int(tltrY)), (int(blbrX), int(blbrY)),
-                (255, 0, 255), 2)
-            cv2.line(converted, (int(tlblX), int(tlblY)), (int(trbrX), int(trbrY)),
-                (255, 0, 255), 2)
-            # distance computation
-            dA = dist.euclidean((tltrX, tltrY), (blbrX, blbrY))
-            dB = dist.euclidean((tlblX, tlblY), (trbrX, trbrY))
-
-            if self.pixel_scale is None:
-                # assuming that always the long edge measurement is supplied
-                self.pixel_scale = max(dA,dB) / self.reference_width
-            # compute the size of the object
-            dimA = dA / self.pixel_scale
-            dimB = dB / self.pixel_scale
-
-        	# draw the object sizes on the image
-            cv2.putText(converted, '{:.3f}m'.format(dimA),
-                (int(tltrX - 15), int(tltrY - 10)), cv2.FONT_HERSHEY_SIMPLEX,
-                0.65, (0, 255, 255), 2)
-            cv2.putText(converted, '{:.3f}m'.format(dimB),
-                (int(trbrX + 10), int(trbrY)), cv2.FONT_HERSHEY_SIMPLEX,
-                0.65, (170, 255, 255), 2)
-
+    def estimate_scale(self, image):
+            self.pixel_scale, color_image = scale_estimation(image, self.reference_width)
             cv2.namedWindow(self.result_window, cv2.WINDOW_AUTOSIZE)
-            cv2.imshow(self.result_window, converted)
+            cv2.imshow(self.result_window, color_image)
 
     def run(self):
         while not self.done and not self.quit:
@@ -208,6 +141,7 @@ class Scaler:
         self.vc.release()
         return self.pixel_scale
 
+
 class MemberInitializer:
     def __init__(self, cam_src = 0):
         self.color = None
@@ -215,7 +149,6 @@ class MemberInitializer:
         self.quit = False
         self.drag = False
         self.cropped = False
-        self.keys = Keys()
         self.roi = []
         self.image = np.zeros((1,1,1), np.uint8)
         self.main_window = 'Add Member'
@@ -234,15 +167,15 @@ class MemberInitializer:
             self.drag = True
 
         if event == cv2.EVENT_MOUSEMOVE and self.drag:
-            temp_img = self.image.copy()
-            cv2.rectangle(temp_img, self.roi[0], (x,y), (0, 255, 0), 2)
-            cv2.imshow(self.main_window, temp_img)
+            temp_image = self.image.copy()
+            cv2.rectangle(temp_image, self.roi[0], (x,y), (0, 255, 0), 2)
+            cv2.imshow(self.main_window, temp_image)
 
         if event == cv2.EVENT_LBUTTONUP and self.drag:
             self.roi.append((x,y))
-            temp_img = self.image.copy()
-            cv2.rectangle(temp_img, self.roi[0], (x,y), (0, 255, 0), 2)
-            cv2.imshow(self.main_window, temp_img)
+            temp_image = self.image.copy()
+            cv2.rectangle(temp_image, self.roi[0], (x,y), (0, 255, 0), 2)
+            cv2.imshow(self.main_window, temp_image)
             self.drag = False
 
         if event == cv2.EVENT_RBUTTONDOWN:
@@ -252,12 +185,12 @@ class MemberInitializer:
             cv2.destroyWindow(self.cropped_window)
             cv2.destroyWindow(self.color_window)
 
-    def add_padding(self, img, padding=50):
+    def add_padding(self, image, padding=50):
         offset = padding/2
         # background image to show the selected area
-        back = np.zeros((img.shape[0]+padding, img.shape[1]+padding, img.shape[2]), np.uint8)
+        back = np.zeros((image.shape[0]+padding, image.shape[1]+padding, image.shape[2]), np.uint8)
         # overlaying cropped area on background image
-        back[offset:offset+img.shape[0],offset:offset+img.shape[1]] = img
+        back[offset:offset+image.shape[0],offset:offset+image.shape[1]] = image
         return back
 
     def get_member_color(self):
@@ -268,7 +201,7 @@ class MemberInitializer:
                 cv2.setMouseCallback(self.main_window, self.mouse_callback)
                 k = cv2.waitKey(50)
                 crop = self.image
-                if k == self.keys.space or self.cropped:
+                if k == Keys.space or self.cropped:
                     self.cropped = True
                     if len(self.roi) == 2:
                         (r1,r2),(r3,r4) = self.roi
@@ -282,36 +215,37 @@ class MemberInitializer:
                         cv2.imshow(self.cropped_window, crop_pad)
 
                 if len(self.roi) == 2:
-                    temp_img = self.image.copy()
-                    cv2.rectangle(temp_img, self.roi[0], self.roi[1], (0, 0, 255), 2)
-                    cv2.imshow(self.main_window, temp_img)
+                    temp_image = self.image.copy()
+                    cv2.rectangle(temp_image, self.roi[0], self.roi[1], (0, 0, 255), 2)
+                    cv2.imshow(self.main_window, temp_image)
                 else:
                     cv2.imshow(self.main_window, self.image)
 
-                if k == self.keys.c and self.cropped:
+                if k == Keys.c and self.cropped:
                     c = cv2.mean(crop)
-                    color_img = crop
-                    color_img[:] = (c[0],c[1],c[2])
-                    color_pad = self.add_padding(color_img)
+                    color_image = crop
+                    color_image[:] = (c[0],c[1],c[2])
+                    color_pad = self.add_padding(color_image)
                     cv2.namedWindow(self.color_window, cv2.WINDOW_AUTOSIZE)
                     cv2.imshow(self.color_window, color_pad)
                     self.color = c
                     print('Average BGR for member id: {}'.format(self.color))
 
-                if k == self.keys.enter:
+                if k == Keys.enter:
                     if self.color:
                         self.destroy_windows()
                         self.done = True
                     else:
-                        print('Operation not done yet.') 
+                        print('Operation not done yet.')
 
-                if k == self.keys.esc:
+                if k == Keys.esc:
                     self.destroy_windows()
                     self.quit = True
             else:
                 print('vc not opened')
         self.vc.release()
         return self.color
+
 
 class Member:
     members = int(0)
--- scripts/ui.py
+++ scripts/ui.py
@@ -3,59 +3,63 @@ import cv2
 from cyclops import Scaler
 from cyclops import Member
 
-ui = np.zeros((540,300,3),np.uint8)
-buttons = [((40,40), (260,100)), ((40,140), (260,200)), ((40,240), (260,300)), ((40,340), (260,400))]
-color_area = [((50,460),(110,520)),((120,460),(180,520)),((190,460),(250,520))]
-text = ['Get Scale','Add Member','Initialize','Start!']
-ranges = [range(40,100), range(140,200), range(240,300), range(340,400)]
-toffs = [(85,80),(65,180),(85,280),(110,380)]
-font = cv2.FONT_HERSHEY_TRIPLEX
-members = []
 
-def buttonCallback(event, x, y, flags, param):
-    global ui, nmembers
-    if event == cv2.EVENT_LBUTTONUP:
-        if x in range(40,260) and y in ranges[0]:
-            sc = Scaler(0, 'param/trust.yaml')
-            s = sc.run()
-            print('{} px/meter'.format(s))
-            button_color = (125, 0, 125) if s else (0, 255, 0)
-            cv2.rectangle(ui, (buttons[0][0]), (buttons[0][1]), button_color, -1)
-            cv2.putText(ui, text[0], toffs[0], font, 0.8, (0,0,0), 2)
-        if x in range(40,260) and y in ranges[1]:
-            if len(members) in range(0,3):
-                m = Member()
-                col = m.get_color()
-                if col:
-                    members.append(m)
-                button_color = (125, 0, 125) if len(members) in range(1,4) else (0, 255, 0)
-                cv2.rectangle(ui, (buttons[1][0]), (buttons[1][1]), button_color, -1)
-                cv2.putText(ui, text[1], toffs[1], font, 0.8, (0,0,0), 2)
-                for mb in range(0,len(members)):
-                    c = members[mb].color
-                    cv2.rectangle(ui, (color_area[mb][0]), (color_area[mb][1]), c, -1)
-            else:
-                print('Maximum 3 members can be added.')
+class UserInterface(object):
 
+    def __init__(self):
+        self.ui = np.zeros((540,300,3),np.uint8)
+        self.buttons = [((40,40), (260,100)), ((40,140), (260,200)), ((40,240), (260,300)), ((40,340), (260,400))]
+        self.color_area = [((50,460),(110,520)),((120,460),(180,520)),((190,460),(250,520))]
+        self.text = ['Get Scale','Add Member','Initialize','Start!']
+        self.ranges = [range(40,100), range(140,200), range(240,300), range(340,400)]
+        self.text_offset = [(85,80),(65,180),(85,280),(110,380)]
+        self.font = cv2.FONT_HERSHEY_TRIPLEX
+        self.members = []
 
-def UI():
-    global ui
-    wname = 'Cyclops'
-    cv2.namedWindow(wname,cv2.WINDOW_AUTOSIZE)
-    cv2.setMouseCallback(wname, buttonCallback)
-    ui[:] = (100,100,100)
-    ui[-100:] = (50, 50, 50)
-    cv2.rectangle(ui, (buttons[0][0]), (buttons[0][1]), (0, 255, 0), -1)
-    cv2.rectangle(ui, (buttons[1][0]), (buttons[1][1]), (0, 255, 0), -1)
-    cv2.rectangle(ui, (buttons[2][0]), (buttons[2][1]), (0, 255, 0), -1)
-    cv2.rectangle(ui, (buttons[3][0]), (buttons[3][1]), (0, 255, 0), -1)
-    cv2.putText(ui, text[0], toffs[0], font, 0.8, (0,0,0), 2)
-    cv2.putText(ui, text[1], toffs[1], font, 0.8, (0,0,0), 2)
-    cv2.putText(ui, text[2], toffs[2], font, 0.8, (0,0,0), 2)
-    cv2.putText(ui, text[3], toffs[3], font, 0.8, (0,0,0), 2)
+    def buttonCallback(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONUP:
+            if x in range(40,260) and y in self.ranges[0]:
+                sc = Scaler(0, '/home/vetenskap/workspace/cyclops/param/trust.yaml')
+                s = sc.run()
+                print('{} px/meter'.format(s))
+                button_color = (125, 0, 125) if s else (0, 255, 0)
+                cv2.rectangle(self.ui, (self.buttons[0][0]), (self.buttons[0][1]), button_color, -1)
+                cv2.putText(self.ui, self.text[0], self.text_offset[0], self.font, 0.8, (0,0,0), 2)
 
-    while True:
-        cv2.imshow(wname, ui)
-        cv2.waitKey(50)
+            if x in range(40,260) and y in self.ranges[1]:
+                if len(self.members) in range(0,3):
+                    m = Member()
+                    col = m.get_color()
+                    if col:
+                        self.members.append(m)
+                    button_color = (125, 0, 125) if len(self.members) in range(1,4) else (0, 255, 0)
+                    cv2.rectangle(self.ui, (self.buttons[1][0]), (self.buttons[1][1]), button_color, -1)
+                    cv2.putText(self.ui, self.text[1], self.text_offset[1], self.font, 0.8, (0,0,0), 2)
+                    for mb in range(0,len(self.members)):
+                        c = self.members[mb].color
+                        cv2.rectangle(self.ui, (self.color_area[mb][0]), (self.color_area[mb][1]), c, -1)
+                else:
+                    print('Maximum 3 members can be added.')
 
-UI()
+    def run(self):
+        window_name = 'Cyclops'
+        cv2.namedWindow(window_name,cv2.WINDOW_AUTOSIZE)
+        cv2.setMouseCallback(window_name, self.buttonCallback)
+        self.ui[:] = (100,100,100)
+        self.ui[-100:] = (50, 50, 50)
+        cv2.rectangle(self.ui, (self.buttons[0][0]), (self.buttons[0][1]), (0, 255, 0), -1)
+        cv2.rectangle(self.ui, (self.buttons[1][0]), (self.buttons[1][1]), (0, 255, 0), -1)
+        cv2.rectangle(self.ui, (self.buttons[2][0]), (self.buttons[2][1]), (0, 255, 0), -1)
+        cv2.rectangle(self.ui, (self.buttons[3][0]), (self.buttons[3][1]), (0, 255, 0), -1)
+        cv2.putText(self.ui, self.text[0], self.text_offset[0], self.font, 0.8, (0,0,0), 2)
+        cv2.putText(self.ui, self.text[1], self.text_offset[1], self.font, 0.8, (0,0,0), 2)
+        cv2.putText(self.ui, self.text[2], self.text_offset[2], self.font, 0.8, (0,0,0), 2)
+        cv2.putText(self.ui, self.text[3], self.text_offset[3], self.font, 0.8, (0,0,0), 2)
+
+        while True:
+            cv2.imshow(window_name, self.ui)
+            cv2.waitKey(50)
+
+if __name__=="__main__":
+    interface = UserInterface()
+    interface.run()

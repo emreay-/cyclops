ac76fbe86b40a65aa541aa742f97ee17c46fead1
Emre <eay@kth.se>
2018-02-03 11:40:21 +0000

    Unit test files

---
+++ cyclops/test/test_user_interface.py
@@ -0,0 +1,8 @@
+import os
+import sys
+import unittest
+import tempfile
+import shutil
+
+from cyclops.test.testing_utility import CyclopsUnitTestBase
+
---
+++ cyclops/test/testing_utility.py
@@ -0,0 +1,11 @@
+import os
+import sys
+import unittest
+import tempfile
+import shutil
+
+
+class CyclopsUnitTestBase(unittest.TestCase):
+
+    pass
+

80b73b98dd834178a8395bafd36e7c7cae04b64a
Emre <eay@kth.se>
2018-02-03 22:44:32 +0000

    Generating dummy file for unit tests

--- cyclops/test/testing_utility.py
+++ cyclops/test/testing_utility.py
@@ -9,3 +9,13 @@ class CyclopsUnitTestBase(unittest.TestCase):
 
     pass
 
+def write_dummy_parameters_file(*args):
+    for arg in args:
+        with open(arg, 'w') as handle:
+            handle.writelines('''{
+    "number_of_particles": 1000,
+    "process_covariance": [1e-2, 1e-2, 1e-2],
+    "measurement_covariance": [1e-2, 1e-2, 1e-2],
+    "reference_distance": 5.0
+ }''')
+ 
